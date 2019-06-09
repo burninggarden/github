6bddd543bb3ae136b915db88c5e3df4acc8c18ca
pachet <pachet@burninggarden.com>
2019-06-09 14:17:46 -0400

    More work on request dispatch

--- src/request.ts
+++ src/request.ts
@@ -1,12 +1,27 @@
 import Payload  from 'interfaces/request-payload';
-import {Method} from '@burninggarden/http';
+import {TimeInterval} from '@burninggarden/enums';
+import {
+	OutgoingRequest,
+	Method
+} from '@burninggarden/http';
 
 abstract class Request<T> {
 
 	public perform(): Promise<T> {
-		throw new Error('die');
+		return this.sendRequest().then(this.parseResponse);
 	}
 
+	private sendRequest(): Promise<any> {
+		const request = new OutgoingRequest({
+			url:     this.getUrl(),
+			method:  this.getMethod(),
+			payload: this.getPayload()
+		});
+
+		return request.send();
+	}
+
+	protected abstract parseResponse(): T;
 	protected abstract getPayload(): Payload;
 	protected abstract getMethod(): Method;
 	protected abstract getUrl(): string;
